6421f3a9ccd60edc316309fa6410d3348f6d4bc3
Ryan McLean and Tate Easterbrooks <ryan@>
2019-01-23 13:26:46 -0800

    setup initial html, css, and javascript files

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+});
+
+
+
+
+
+//
+//   var total = 5;
+//   for (var currentNumber = 5; currentNumber <= 5; currentNumber += 1) {total += currentNumber; }
+//   console.log(total)
+
+// var total = 2;
+// for (var currentNumber = 1; currentNumber <= 50; currentNumber *= 2) {
+//   console.log(currentNumber)
+// // }
+// var quote = ("Believe you can and you're halfway there. Theodore Roosevelt");
+// console.log(quote);
